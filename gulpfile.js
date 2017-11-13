e28a6e35159d51c4065f968d943947ea20632eaa
Matt Dunn <matt@processyellow.ca>
2017-11-13 10:38:47 -0500

    spacing

--- gulpfile.js
+++ gulpfile.js
@@ -8,14 +8,14 @@
 
 */
 
-var browserSync = require('browser-sync');
-var cp  		= require('child_process');
-var gulp        = require('gulp');
-var rename 		= require('gulp-rename');
-var sass        = require('gulp-sass');
-var prefix      = require('gulp-autoprefixer');
-var uglify      = require('gulp-uglify');
-var concat      = require('gulp-concat');
+var browserSync  = require('browser-sync');
+var cp  = require('child_process');
+var gulp  = require('gulp');
+var rename  = require('gulp-rename');
+var sass  = require('gulp-sass');
+var prefix  = require('gulp-autoprefixer');
+var uglify  = require('gulp-uglify');
+var concat  = require('gulp-concat');
 
 gulp.task('browser-sync', ['styles', 'jekyll-build'], function() {
 	browserSync(
@@ -37,25 +37,25 @@ gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
 
 gulp.task('styles', function () {
 	return gulp.src('./src/*.scss')
-		.pipe(sass({outputStyle: 'compact'}).on('error', sass.logError))
-		.pipe(rename('mattdoesthings.css'))
-		.pipe(gulp.dest('_site/')) // browser-sync live injection
-		.pipe(browserSync.reload({stream:true}))
-		.pipe(gulp.dest('./'));
+	.pipe(sass({outputStyle: 'compact'}).on('error', sass.logError))
+	.pipe(rename('mattdoesthings.css'))
+	.pipe(gulp.dest('_site/')) // browser-sync live injection
+	.pipe(browserSync.reload({stream:true}))
+	.pipe(gulp.dest('./'));
 });
 
 gulp.task('js', function() {
 	return gulp.src([
-			'./node_modules/jquery/jquery.js',
-			'./node_modules/slick-carousel/slick/slick.js',
-			'./node_modules/remodal/dist/remodal.js',
-			'./src/scripts.js'
-		])
-		.pipe(concat('mattdoesthings.js'))
-		// .pipe(uglify())
-		.pipe(gulp.dest('_site/'))
-		.pipe(browserSync.reload({stream: true}))
-		.pipe(gulp.dest('./'))
+		'./node_modules/jquery/jquery.js',
+		'./node_modules/slick-carousel/slick/slick.js',
+		'./node_modules/remodal/dist/remodal.js',
+		'./src/scripts.js'
+	])
+	.pipe(concat('mattdoesthings.js'))
+	// .pipe(uglify())
+	.pipe(gulp.dest('_site/'))
+	.pipe(browserSync.reload({stream: true}))
+	.pipe(gulp.dest('./'))
 });
 
 gulp.task('watch', function() {
